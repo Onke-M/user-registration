417002324a7df0f1d145737535948528ba533637
Onke-M <56151334+OnkeMweza@users.noreply.github.com>
2022-06-04 22:17:11 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { OtpPageComponent } from './otp/otp-page/otp-page.component';
+import { RegisterPageComponent } from './register/register-page/register-page.component';
+import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',component: LoginPageComponent},
+  { path: 'login',component: LoginPageComponent},
+  { path: 'register', component: RegisterPageComponent},
+  { path: 'dashboard', component: DashboardPageComponent},
+  { path: 'otp', component: OtpPageComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { RegisterPageComponent } from './register/register-page/register-page.component';
+import { OtpPageComponent } from './otp/otp-page/otp-page.component';
+import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    OtpPageComponent,
+    DashboardPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
