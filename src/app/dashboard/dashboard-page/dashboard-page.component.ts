eb4bdb400df593495bb175eaa138f8a83915f50f
Onke-M <56151334+OnkeMweza@users.noreply.github.com>
2022-07-02 17:53:20 +0200

    added dashboard with charts

--- src/app/dashboard/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/dashboard-page/dashboard-page.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatTableDataSource } from '@angular/material/table';
 import { environment } from 'src/environments/environment';
+import { ChartData, ChartOptions} from 'chart.js';
 
 
 const API_URL = environment.API_URL;
-
+var nikeCount: number
+var adidasCount: number
+var leviCount: number
 export interface Product {
   name:string,
   price:number,
@@ -21,8 +24,42 @@ export interface Product {
 })
 export class DashboardPageComponent implements OnInit {
   products: any = [];
-  displayedColumns: string[] = ['Name', 'Price', 'Description'];
+  displayedColumns: string[] = ['Name', 'Price', 'Brand', 'Type', 'Description'];
   dataSource = new MatTableDataSource<Product>()
+
+  brandData: ChartData<'bar'> = {
+    labels: ['Nike', 'Adidas', 'Levi Strauss'],
+    datasets: [
+      { data: [7, 5, 3] },
+    ],
+  };
+
+  brandChartOptions: ChartOptions = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Product Count by Brands',
+      },
+    },
+  };
+
+  productTypeData: ChartData<'pie'> = {
+    labels: ['Footwear', 'Clothing', 'Accessories'],
+    datasets: [
+      { data: [7, 2, 6] },
+    ],
+  };
+
+  productTypeChartOptions: ChartOptions = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Product Count by Product Type',
+      },
+    },
+  };
   constructor(public http: HttpClient) { }
 
   ngOnInit(){
